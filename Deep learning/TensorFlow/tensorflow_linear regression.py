58948903f9ba0328815d0881bc7e86f9d67ea681
mayanks888 <mayanksati07@gmail.com>
2018-04-23 19:05:16 +0530

    created in office

--- Deep learning/TensorFlow/tensorflow_linear regression.py
+++ Deep learning/TensorFlow/tensorflow_linear regression.py
@@ -8,25 +8,32 @@ ylabel=np.linspace(0,10,10)+np.random.uniform(-1.5,1.5,10)
 m=tf.Variable(initial_value=.44)
 b=tf.Variable(initial_value=.87)
 error=0
+note=tf.Variable(initial_value=35)
 for x,y in zip(xlabel,ylabel):
     y_val=m*x+b
     error+=(y_val-y)**2
+    
+
 
 optimiser=tf.train.GradientDescentOptimizer(learning_rate=.001)
-train_data=optimiser.minimize(error)
+train_data=optimiser.minimize(error,note)
 init=tf.global_variables_initializer()
 
 sess=tf.Session()
 sess.run(init)
-training_steps=20
+training_steps=5
 
 for i in range(training_steps):
     sess.run(train_data)
-    
-final_slope,final_intercept=sess.run([m,b])
+    final_slope,final_intercept=sess.run([m,b])
+    print (sess.run([m,b,note]))
 
-xtest=np.linspace(-1,11,10)
-y_predict=final_slope*xtest+final_intercept
-plt.plot(xtest,y_predict)
-plt.scatter(xlabel,ylabel)
-plt.show()
+    #this is for test datasets accuracy
+    xtest=np.linspace(-1,11,10)
+    y_predict=final_slope*xtest+final_intercept
+    plt.plot(xtest,y_predict)
+    plt.scatter(xlabel,ylabel)
+    plt.show()
+    print (sess.run(error))
+    
+
