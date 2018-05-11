3be9f3473b84d8a2a3dc30d95ba34b91c4ba45d8
mayanks888 <mayanksati07@gmail.com>
2018-05-11 20:09:17 +0530

    made some changes in home

--- Deep learning/TensorFlow/tensorflow_practise.py
+++ Deep learning/TensorFlow/tensorflow_practise.py
@@ -10,17 +10,21 @@ z = y + 3
 init=tf.global_variables_initializer()
 sess=tf.Session()
 sess.run(init)
-
+sess = tf_debug.TensorBoardDebugWrapperSession(sess, "MAYANK_PC:7000")
+# sess = tf_debug.TensorBoardDebugWrapperSession(sess, "MAYANK_PC:6064")
+# sess.run(my_fetches)
 # sess = tf_debug.TensorBoardDebugWrapperSession(sess, "http://0.0.0.0:6006/#debugger")
 
 # sess.run(my_fetches)
 x_data = [[1, 2, 3],
          [4, 5, 6],]
 result = sess.run(z, feed_dict={x: x_data})
-sess = tf_debug.TensorBoardDebugWrapperSession(sess, "mayank:7000")
+# sess = tf_debug.TensorBoardDebugWrapperSession(sess, "MAYANK_PC:7000")
 # sess.run(my_fetches)
 # new_result=sess.run(y,feed_dict={x: x_data})
 # print(sess.run(x))
 
 print(result)
 # print (new_result)
+
+# python "Deep learning\TensorFlow\tensorflow_practise.py"
