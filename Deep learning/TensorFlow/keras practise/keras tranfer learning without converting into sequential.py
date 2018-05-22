7720c5eead9e2b0bee5ee635c80cc2259eb7193e
mayanks888 <mayanksati07@gmail.com>
2018-05-22 10:29:56 +0530

    changed in office

--- Deep learning/TensorFlow/keras practise/keras tranfer learning without converting into sequential.py
+++ Deep learning/TensorFlow/keras practise/keras tranfer learning without converting into sequential.py
@@ -89,12 +89,12 @@ test_validation = test_datagen.flow_from_directory("../../../Datasets/cat_dogs/t
 
 # keras_backend.set_session(tf_debug.TensorBoardDebugWrapperSession(tf.Session(),"localhost:7000"))
 #linux
-# cb=TensorBoard(log_dir=("/home/mayank-s/PycharmProjects/Data_Science/output_graph/try4"))#,histogram_freq = 1, batch_size = 32,write_graph ="TRUE" )
+cb=TensorBoard(log_dir=("/home/mayank-s/PycharmProjects/Data_Science/output_graph/try4"))#,histogram_freq = 1, batch_size = 32,write_graph ="TRUE" )
 # @windows
-cb=TensorBoard(log_dir=("C:/Users/mayank/Documents/Data_Science/output_graph/try5"))
+# cb=TensorBoard(log_dir=("C:/Users/mayank/Documents/Data_Science/output_graph/try5"))
 
 custom_vgg_model2.fit_generator(generator=training_data,
-                            steps_per_epoch=500,
+                            steps_per_epoch=15,
                             epochs=5,
                             validation_data=test_validation,
                             validation_steps=5,callbacks=[cb])
@@ -107,7 +107,7 @@ from keras.preprocessing import image
 
 # test_image=image.load_img(path="C:/Users/mayank/Documents/Datasets/Cat_dogs/test1/509.jpg",target_size=(64,64))
 #linux path
-test_image=image.load_img(path="../../../Datasets/cat_dogs/test1/525.jpg",target_size=(224,224))
+test_image=image.load_img(path="../../../Datasets/cat_dogs/test1/85.jpg",target_size=(224,224))
 test_image=image.img_to_array(test_image)
 test_image=np.expand_dims(test_image,axis=0)
 result=custom_vgg_model2.predict(test_image)
