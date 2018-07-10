f05c9f1a99e513457675a00aea2320cf3ce64f30
mayanks888 <mayanksati07@gmail.com>
2018-07-10 18:54:33 +0530

    new

--- Deep learning/Load Datasets/read_any_datasets_csv.py
+++ Deep learning/Load Datasets/read_any_datasets_csv.py
@@ -1,20 +1,29 @@
 import pandas as pd
 from batchup import data_source
+import random
+from sklearn.utils import shuffle
+
 import numpy as np
 df=pd.read_csv("/home/mayank-s/PycharmProjects/Datasets/pascal_voc_csv.csv")
 print(df.head())
 print (df.info())
-#new_data= (df.groupby('image_name'))
-#print (new_data)
+
+df=df.iloc[np.random.permutation(len(df))]
+print (df.head())
 grouped = df.groupby('image_name')
 bbox=[]
 label=[]
 image_name=[]
 loop=0
-for name,group in grouped:
+
+# lis=[1,2,3,4,5,6]
+# # random.shuffle(lis)
+# for val in (random.shuffle(lis)):
+#     print (val)
+for name,group in (grouped):
     loop+=1
     print (name)
-    image_name.append(name)
+    # image_name.append(name)
     # print (group)
     # print(group.iloc[0])
     # print (group.values)
@@ -26,41 +35,22 @@ for name,group in grouped:
     print(image_name)
     print (bbox)
     print(label)
-    sample_index = np.random.choice(sample_number,batch_size,replace=True)
-    if loop>100:
+    if loop>500:
         break
 
 
 
 print (bbox.__sizeof__())    #print(group.xmax.values)
-ds = data_source.ArrayDataSource([image_name,bbox])
-
-for (im, bb) in ds.batch_iterator(batch_size=10, shuffle=True):
-    print(im,bb)
 
 
-
-
-batch
-
-
-
-
-
-
-
-
-batch
-
-
-
-
-
-
-
-
+sample_number= df.shape[0]
+batch_size=1
 batches = sample_number // batch_size
-        for i in range(batches):
-            images = []
-            boxes = []
-            sample_index = np.random.choice(sample_number,batch_size,replace=True)
+for i in range(batches):
+    images = []
+    boxes = []
+    sample_index = np.random.choice(sample_number,batch_size,replace=True)
+#ds = data_source.ArrayDataSource([image_name,bbox])
+print(i)
+'''for (im, bb) in ds.batch_iterator(batch_size=10, shuffle=True):
+    print(im,bb)'''
